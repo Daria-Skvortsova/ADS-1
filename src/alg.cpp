7ce9d4299b39a91d39f1d2b8f2275f9f7aa886cf
Daria-Skvortsova <99176875+Daria-Skvortsova@users.noreply.github.com>
2022-02-21 23:06:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,77 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  int x, i, count;
+  x = value;
+  count = 0;
+  for (i = 1; i <= sqrt(x); i++) {
+    if (x % i == 0) {
+      count += 1;
+    }
+  }
+  if (count != 1) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int x, i, j, count, count_global;
+  x = n;
+  x = 0;
+  count_global = 0;
+  for (j = 2; x != count_global; j++) {
+    count = 0;
+    for (i = 2; i <= sqrt(j); i++) {
+      if (j % i == 0) {
+        count += 1;
+        break;
+      }
+    }
+    if (count == 2) {
+      count_global += 1;
+    }
+  }
+  return j-1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int x, i, j, count, count_global;
+  x = value;
+  x = 0;
+  count_global = 1;
+  for (j = x + 1; count_global == 1; j++) {
+    count = 0;
+	for (i = 2; i <= sqrt(j); i++) {
+	  if (j % i == 0) {
+	    count += 1;
+	  }
+	}
+	if (count == 0) {
+	  count_global = 0;
+	  break;
+	}
+  }
+  return j;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+int x,i,j,count,symma;
+  symma = 0;
+  x = hbound;
+  x = 0;
+  for (j = 2; j != x; j++) {
+    count = 0;
+	for (i = 2; i <= sqrt(j); i++) {
+	  if (j % i == 0) {
+	    count += 1;
+		break;
+      }
+	}
+	if (count == 0) {
+	  symma += j;
+	}
+  }
+  return symma;
 }
